8a6a41884fd32411ee96ce1a3350cbcb28aec8ef
HARSHKALAL <kalalharsh708@gmail.com>
2023-04-26 14:48:24 +0530

    chat template file created

--- Chatify/chat/urls.py
+++ Chatify/chat/urls.py
@@ -1,7 +1,7 @@
-from .views import RegisterView
-from django.urls import include, path
+from django.urls import path
+from .views import *
 
-app_name = 'chat'
+
 urlpatterns = [
-    path('user_register/', RegisterView.as_view(), name='user_register'),
+    path("chatui/",chat,name="chatui"),
 ]
--- Chatify/chat/views.py
+++ Chatify/chat/views.py
@@ -1,12 +1,4 @@
-from rest_framework.views import APIView
-from rest_framework import status
-from rest_framework.renderers import TemplateHTMLRenderer
-from rest_framework.response import Response
+from django.shortcuts import render
 
-
-class RegisterView(APIView):
-    renderer_classes = [TemplateHTMLRenderer]
-    template_name = 'chat/register.html'
-
-    def get(self, request, *args, **kwargs):
-        return Response(status=status.HTTP_200_OK)
+def chat(request):
+    return render(request,"chat/chat.html")
