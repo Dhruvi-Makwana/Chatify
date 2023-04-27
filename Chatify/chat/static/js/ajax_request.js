6469a9463233597f0957c54f43009d93737114c3
Dhruvi Makwana <dhruvi.makwana@trootech.com>
2023-04-27 19:18:51 +0530

    make base file

--- Chatify/chat/static/js/ajax_request.js
+++ Chatify/chat/static/js/ajax_request.js
@@ -1,8 +1,3 @@
-var csrfToken = null
-
-function csrfFunc(token) {
-    csrfToken = token
-}
 function makeAjaxRequest(methodType, csrfToken, url, data, callback)
 {
     $.ajax({
@@ -27,22 +22,3 @@ function makeAjaxRequest(methodType, csrfToken, url, data, callback)
     });
 }
 
- $(document).on("submit", "#register_form", function(event) {
-        console.log("hii")
-        event.preventDefault();
-        event.stopImmediatePropagation();
-        var formdata =  new FormData(this)
-        makeAjaxRequest('POST',csrfToken,"/register/", formdata, function(response){
-        console.log("register")
-        })
-    })
-
- $(document).on("submit", "#login_form", function(event) {
-        console.log("hii")
-        event.preventDefault();
-        event.stopImmediatePropagation();
-        var formdata =  new FormData(this)
-        makeAjaxRequest('POST',csrfToken,"/login/", formdata, function(response){
-            window.location = '/login/';
-        })
-    })
---
+++ Chatify/chat/static/js/register.js
@@ -0,0 +1,10 @@
+
+$(document).on("submit", "#register_form", function(event) {
+        console.log("hii")
+        event.preventDefault();
+        event.stopImmediatePropagation();
+        var formdata =  new FormData(this)
+        makeAjaxRequest('POST',csrfToken,"/register/", formdata, function(response){
+        console.log("register")
+        })
+    })
