49baf4cf2c9268b62e7bfd6267bbc2089659fd87
HARSHKALAL <kalalharsh708@gmail.com>
2023-05-08 11:05:44 +0530

    [FIXED] bug for mobile number

--- Chatify/chat/utils.py
+++ Chatify/chat/utils.py
@@ -1,5 +1,7 @@
 def set_contact_number(data):
     get_mboile_number = data.get("mobile_number")
-    if get_mboile_number[:3] != "+91":
+    if len(get_mboile_number) > 10 and get_mboile_number[:2] == "91":
+        data["mobile_number"] = "+" + get_mboile_number
+    elif get_mboile_number[:3] != "+91":
         data["mobile_number"] = "+91" + get_mboile_number
     return data
