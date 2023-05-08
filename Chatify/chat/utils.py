7f1a5000b3163db3351c4b3a95b205151d92d4d1
HARSHKALAL <kalalharsh708@gmail.com>
2023-05-08 15:10:24 +0530

    [FIXED] All scenario in utils file

--- Chatify/chat/utils.py
+++ Chatify/chat/utils.py
@@ -1,7 +1,16 @@
+from rest_framework import serializers
+from rest_framework import status
+
+
 def set_contact_number(data):
-    get_mboile_number = data.get("mobile_number")
-    if len(get_mboile_number) > 10 and get_mboile_number[:2] == "91":
-        data["mobile_number"] = "+" + get_mboile_number
-    elif get_mboile_number[:3] != "+91":
-        data["mobile_number"] = "+91" + get_mboile_number
+    get_mobile_number = data.get("mobile_number")
+    if len(get_mobile_number) > 10 and len(get_mobile_number) <= 13:
+        if get_mobile_number[:2] == "91":
+            data["mobile_number"] = "+" + get_mobile_number
+        elif get_mobile_number[:3] != "+91":
+            raise serializers.ValidationError(
+                {"phone_number": "Phone number is Not Valid"}
+            )
+    elif len(get_mobile_number) == 10:
+        data["mobile_number"] = "+91" + get_mobile_number
     return data
