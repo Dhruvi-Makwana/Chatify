0781ea712240c8c70e121d3c504c609885c363b1
Dhruvi Makwana <dhruvi.makwana@trootech.com>
2023-05-18 15:04:02 +0530

    afterlogin userlist updated using consumer

--- Chatify/chat/consumers.py
+++ Chatify/chat/consumers.py
@@ -8,24 +8,28 @@ class VisibilityStatusConsumer(AsyncJsonWebsocketConsumer):
         await self.accept()
 
     async def receive_json(self, event):
+
+        user_id = event.get("Userid")
         await self.channel_layer.group_send(
             "visiblity-group",
             {
                 "type": "chat.message",
+                "id": user_id,
             },
         )
 
     @sync_to_async
-    def updated_instance(self):
+    def updated_instance(self, user_id):
         from .models import User
 
-        instance = User.objects.get(id=self.scope["user"].id)
+        instance = User.objects.get(id=user_id)
         return instance
 
     async def chat_message(self, event):
         from .serializers import UserSerializer
 
-        modify_instance = await self.updated_instance()
+        userid = event.get("id")
+        modify_instance = await self.updated_instance(userid)
         serializer = UserSerializer(instance=modify_instance)
         await self.send_json(serializer.data),
 
