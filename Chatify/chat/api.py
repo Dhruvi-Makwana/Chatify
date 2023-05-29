94911b22cb27855edb44280823de1095c7ead6f8
HARSHKALAL <kalalharsh708@gmail.com>
2023-05-29 12:59:12 +0530

    BUGS FIXED OF register

--- Chatify/chat/api.py
+++ Chatify/chat/api.py
@@ -69,7 +69,6 @@ class LoginAPIView(APIView):
 
 class VisibilityStatusAPI(APIView):
     def post(self, request, *args, **kwargs):
-
         user_id = request.data.get("id")
         get_status = request.data.get("status")
         set_status(user_id, get_status)
@@ -79,7 +78,11 @@ class VisibilityStatusAPI(APIView):
 
 class OnlineUsersAPI(APIView):
     def get(self, request):
-        data = User.objects.filter(is_online=True, is_active=True).order_by("-id").exclude(id=request.user.id)
+        data = (
+            User.objects.filter(is_online=True, is_active=True)
+            .order_by("-id")
+            .exclude(id=request.user.id)
+        )
         return JsonResponse(
             {"UserData": list(UserSerializer(data, many=True).data)},
         )
@@ -89,7 +92,7 @@ class LogoutView(APIView):
     def get(self, request):
         user_session_key = request.session.session_key
         Session.objects.filter(session_key__startswith=user_session_key).delete()
-        send_chat_message(set_status(request.user, "online"), "logout")
+        send_chat_message(set_status(request.user.id, "online"), "logout")
         logout(request)
         return redirect(reverse("chat:loginUI"))
 
@@ -108,12 +111,6 @@ class OnlineUsersAPI(APIView):
             .exclude(id=request.user.id)
         )
         return JsonResponse({"UserData": list(UserSerializer(data, many=True).data)})
-
-
-class LogoutView(APIView):
-    def get(self, request):
-        logout(request)
-        return redirect(reverse("chat:loginUI"))
 
 
 class ChatMessageListCreateView(generics.ListCreateAPIView):
